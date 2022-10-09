3d66bd9875560aa96ce849f6ff74912d4ab30226
yh.thomas.lee <yh.thomas.lee@gmail.com>
2022-10-09 22:06:10 +0900

    practice commit

--- src/calander/calendarPractice.java
+++ src/calander/calendarPractice.java
@@ -2,21 +2,23 @@ package calander;
 
 import java.util.Scanner;
 
-public class calendar {
+public class calendarPractice {
+	//달력을 만들 재료를 field에 기입
 	static char[] weeks = {'일','월','화','수','목','금','토'};
 	static int[] lastDay = {31,28,31,30,31,30,31,31,30,31,30,31};
+	// field setting 끝
 	
 	private static int getMonth(int year, int month) {
 		if(month<1 || month>12) {
-			System.out.println("[log] month를 잘못입력하였습니다. ");			
-		}else {
-			System.out.println("[log] month : "+month);
+			System.out.println("[log] month를 잘못 입력 했습니다.");
+		} else {
+			System.out.println("[log] month: "+month);
 		}
 		return month;
 	}
 	
 	private static int getFirstDay(int year, int month) {
-		int firstDay = (year-1)*365 + (year-1)/4 - (year-1)/100 + (year-1)/400;
+		int firstDay = (year-1)*365  + (year-1)/ - (year-1)/100+ (year-1)/400;
 		for(int i=0; i<month-1; i++) {
 			firstDay += lastDay[i];
 		}
@@ -24,18 +26,16 @@ public class calendar {
 	}
 	
 	private static int[] getLastDay(int year, int month) {
-		// TODO Auto-generated method stub
 		//윤년체크 포함
-		if(year%4 == 0 && year%100 != 0 || year%400 == 0) {
-			lastDay[1] = 29;
-		}else {
-			lastDay[1] = 28;
+		if(year%4==0 && year%100 !=0 || year%400 ==0) {
+			lastDay[1]=29;
+		} else {
+			lastDay[1]=28;
 		}
 		return lastDay;
 	}
 	
 	private static void makeCalendar(int year, int month) {
-		// TODO Auto-generated method stub
 		System.out.println("year, month "+year+", "+month);
 		
 		int firstDay = getFirstDay(year, month);
@@ -44,23 +44,22 @@ public class calendar {
 		int week = firstDay%7;
 		
 		System.out.printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t\n","월","화","수","목","금","토","일");
-		
-		for(int i=0;i<week;i++) {
+
+		for(int i=0; i<week; i++) {
 			System.out.print("\t");
 		}
-		for(int i=1;i<=lastDay[month-1];i++) {
+		for(int i=1; i<=lastDay[month-1]; i++) {
 			System.out.printf("%d\t",i);
 			week++;
-			if(week%7 == 0)
+			if(week%7==0)
 				System.out.println();
 		}
-		if(week%7 != 0) {
-			System.out.println();
-		}		
-	
+			if(week%7!=0) {
+				System.out.println();
+			}
 	}
-	
-	public static void main(String[]args) {
+
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("please input year and month to verify the calendar.\nYou must input like '2020,2' format.");
 		String inputValue = scanner.nextLine();
@@ -74,3 +73,4 @@ public class calendar {
 	}
 	
 }
+
